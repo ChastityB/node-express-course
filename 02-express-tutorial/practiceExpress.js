bbf10bbe55eaa8ea1323f61b5b2b74b484548e35
Chas <paredeschastity@gmail.com>
2023-01-16 06:15:17 -0500

    answers for lesson 3

--- 02-express-tutorial/practiceExpress.js
+++ 02-express-tutorial/practiceExpress.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static("./new-public"));
+
+app.get("/sample", (req, res) => {
+  res.send("This is working");
+});
+
+app.all("*", (req, res) => {
+  res.status(400).send("Resource Not Found");
+});
 
 app.listen(3000, () => {
-  console.log("Server is listening on port 5000");
+  console.log("Server is listening on port 3000");
 });
